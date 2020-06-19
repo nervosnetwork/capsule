b4451acc0a0286ff5f72e12e41cbb3cebdb5409f
jjy <jjyruby@gmail.com>
2020-06-19 03:33:38 +0000

    Generate more sophisticated rust contract template

--- templates/rust/contract/src/main.rs
+++ templates/rust/contract/src/main.rs
@@ -4,13 +4,69 @@
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
 
-use ckb_std::{entry, default_alloc};
+// Import from `core` instead of from `std` since we are in no-std mode
+use core::result::Result;
 
-entry!(main);
+// Import heap related library from `alloc`
+// https://doc.rust-lang.org/alloc/index.html
+use alloc::{vec, vec::Vec};
+
+// Import CKB syscalls / structures
+use ckb_std::{
+    entry,
+    default_alloc,
+    debug,
+    high_level::{load_script, load_tx_hash},
+    error::SysError,
+    ckb_types::{bytes::Bytes, prelude::*},
+};
+
+entry!(entry);
 default_alloc!();
 
-#[no_mangle]
-fn main() -> i8 {
-    // this contract always return true
-    0
+/// Program entry
+fn entry() -> i8 {
+    // Call main function and return error code
+    match main() {
+        Ok(_) => 0,
+        Err(err) => err as i8,
+    }
+}
+
+/// Error
+#[repr(i8)]
+enum Error {
+    IndexOutOfBound = 1,
+    ItemMissing,
+    LengthNotEnough,
+    Encoding,
+    // Add customized errors here...
+}
+
+impl From<SysError> for Error {
+    fn from(err: SysError) -> Self {
+        use SysError::*;
+        match err {
+            IndexOutOfBound => Self::IndexOutOfBound,
+            ItemMissing => Self::ItemMissing,
+            LengthNotEnough(_) => Self::LengthNotEnough,
+            Encoding => Self::Encoding,
+            Unknown(err_code) => panic!("unexpected sys error {}", err_code),
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    // remove below examples and write your code here
+
+    let script = load_script()?;
+    let args: Bytes = script.args().unpack();
+    debug!("script args is {:?}", args);
+
+    let tx_hash = load_tx_hash()?;
+    debug!("tx hash is {:?}", tx_hash);
+
+    let _buf: Vec<_> = vec![0u8; 32];
+
+    Ok(())
 }
