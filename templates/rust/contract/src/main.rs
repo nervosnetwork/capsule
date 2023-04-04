bb1e8f79c5ffa2d510e12df355a1dfaaca6c9e83
Yin Guanhao <sopium@mysterious.site>
2023-04-04 16:24:37 +0800

    Update rust template deps

--- templates/rust/contract/src/main.rs
+++ templates/rust/contract/src/main.rs
@@ -6,7 +6,6 @@
 
 #![no_std]
 #![no_main]
-#![feature(asm_sym)]
 #![feature(lang_items)]
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
@@ -15,22 +14,16 @@
 mod entry;
 mod error;
 
-use core::arch::asm;
-use ckb_std::{
-    default_alloc,
-};
+use ckb_std::default_alloc;
 
 ckb_std::entry!(program_entry);
 default_alloc!();
 
 /// program entry
-///
-///  Both `argc` and `argv` can be omitted.
-fn program_entry(_argc: u64, _argv: *const *const u8) -> i8 {
+fn program_entry() -> i8 {
     // Call main function and return error code
     match entry::main() {
         Ok(_) => 0,
         Err(err) => err as i8,
     }
 }
-
