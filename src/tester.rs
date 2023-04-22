08e4da21514e779e022ebf73c0e1073fbfd5947e
Yin Guanhao <sopium@mysterious.site>
2023-04-22 11:41:58 +0800

    feat: use xshell for test
    to improve transparency and simplify code

--- src/tester.rs
+++ src/tester.rs
@@ -1,7 +1,6 @@
-use std::process::Command;
-
 use crate::project_context::{BuildEnv, Context};
 use anyhow::Result;
+use xshell::{cmd, Shell};
 
 const TEST_ENV_VAR: &str = "CAPSULE_TEST_ENV";
 const TESTS_DIR: &str = "tests";
@@ -13,24 +12,15 @@ impl Tester {
             BuildEnv::Debug => "debug",
             BuildEnv::Release => "release",
         };
+        println!("{TEST_ENV_VAR}={env_arg}");
         let workspace_dir = project_context.workspace_dir()?;
         let test_dir = workspace_dir.join(TESTS_DIR);
-        // When workspace_dir is "contracts" we must mount build directory to /code/build so that test Loader can load the binary.
-        let mut cmd = Command::new("cargo");
-        cmd.arg("test")
-            .current_dir(&test_dir)
-            .env(TEST_ENV_VAR, env_arg);
-        if let Some(test_name) = test_name {
-            cmd.arg(test_name);
-        }
-        cmd.arg("--").arg("--nocapture");
-        let status = cmd.status()?;
-        if !status.success() {
-            return Err(anyhow::anyhow!(
-                "cargo test failed: {}",
-                status.code().unwrap_or(-1)
-            ));
-        }
+
+        let sh = Shell::new()?;
+        sh.change_dir(test_dir);
+        cmd!(sh, "cargo test {test_name...} -- --nocapture")
+            .env(TEST_ENV_VAR, env_arg)
+            .run()?;
         Ok(())
     }
 }
